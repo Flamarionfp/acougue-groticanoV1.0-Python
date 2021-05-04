03dc6ad7eed8c6e5737b13d82583c65d4fe05b84
Flamarion Fagundes Pinto <83596149+Flamarionfp@users.noreply.github.com>
2021-05-04 16:54:12 -0300

    Update compradecarne.py

--- compradecarne.py
+++ compradecarne.py
@@ -1,28 +1,33 @@
 from time import sleep
-print('\033[1;34mMercado Groticano\033[m')
+print('Açougue Groticano')
 print('-'*84)
-print('\033[4;32mTabela de preços\033[m')
+print('Tabela de preços')
 print('Filé duplo: R$ 4.90 por kg até 5 kg. R$ 5.80 por kg acima de 5kg')
 print('Alcatra: R$ 5.90 por kg até 5kg. R$ 6.80 por kg acima de 5kg')
 print('Picanha: R$ 6.90 por kg até 5g. R$ 7.80 por kg acima de 5kg')
 print('-'*84)
-print('\033[0;32mOBS:\033[mCompras feitas através do \033[1;32mCartão Groticano\033[m recebem \033[1;32m5% de desconto\033[m sobre o total!')
+print('OBS:Compras feitas através do Cartão Groticano recebem 5% de desconto sobre o total!')
 print('-'*84)
-nome = str(input('Olá, bem vindo ao \033[1;34mMercado Groticano!\033[m Qual o seu nome? ')).strip()
-if len(nome) == 0:
-    print('É necessário que você informe seu nome para que possamos continuar o atendimento')
+nome = str(input('Olá, bem vindo ao Açougue Groticano! Qual o seu nome? ')).strip()
+while len(nome) <= 2:
+    print('Nome inválido! É necessário que você informe seu nome corretamente para que possamos\ncontinuar o atendimento')
+    nome = str(input('Seu nome: ')).strip()
 else:
-    carne = str(input('Qual tipo de carne você deseja {}? '.format(nome))).upper().strip()
+    carne = str(input('Qual tipo de carne você deseja, {}? '.format(nome))).upper().strip()
     preco = 0
-    if carne != 'FILÉ DUPLO' and carne != 'FILE DUPLO' and carne != 'ALCATRA' and carne != 'PICANHA':
-        print(
-            'Não Trabalhamos com o que pediu {}. Você deve informar o tipo de carne desejada\nde acordo com os cortes disponíveis na lista acima.'.format(nome))
+    while carne != 'FILÉ DUPLO' and carne != 'FILE DUPLO' and carne != 'ALCATRA' and carne != 'PICANHA':
+        print('Não Trabalhamos com o que pediu.')
+        print('Você deve informar o tipo de carne desejada de acordo com os cortes disponíveis na lista acima.')
+        carne = str(input('Informe novamente o tipo de carne desejada: ')).upper().strip()
     else:
         quant = float(input('Quantos kg você gostaria? '))
-        if quant <= 0:
+        while quant <= 0:
             print('Erro! A quantidade de kg deve ser maior que 0')
+            quant = float(input('Quantos kg você gostaria? '))
         else:
             pag = int(input('Qual a forma de pagamento? Digite 1 para Dinheiro ou 2 para Cartão Groticano: '))
+            while pag != 1 and pag != 2:
+                pag = int(input('Forma de pagamento inválida, digite novamente: '))
             if pag == 1:
                 forma = 'Dinheiro'
                 if carne == 'FILÉ DUPLO' or carne == 'FILE DUPLO':
@@ -72,11 +77,11 @@ else:
                             preco_total = quant * 7.80 - desconto
                 else:
                     print('Erro, digite uma forma de pagamento válida.')
-            print('\033[0;32mAguarde enquanto emitimos sua nota fiscal...\033[m')
+            print('Aguarde enquanto emitimos sua nota fiscal...')
             sleep(2)
             print('Nome do cliente: {}'.format(nome))
             print('Tipo de carne: {}'.format(carne))
             print('Quantidade de carne: {:.1f} kg'.format(quant))
             print('Forma de pagamento: {}'.format(forma))
             print('Desconto R$ {:.2f}\nTOTAL: R$ {:.2f}'.format(desconto, preco_total))
-            input()
+            input('Aperte enter para sair...')
